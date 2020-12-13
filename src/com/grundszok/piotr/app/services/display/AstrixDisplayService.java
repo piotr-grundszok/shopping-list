7943d60408df88ec73b80415f9607fd08e509d54
Piotr Grundszok <piotr-grundszok@wp.pl>
2020-12-13 13:24:10 +0100

    refactor services to use borders

--- src/com/grundszok/piotr/app/services/display/AstrixDisplayService.java
+++ src/com/grundszok/piotr/app/services/display/AstrixDisplayService.java
@@ -3,8 +3,9 @@ package com.grundszok.piotr.app.services.display;
 public class AstrixDisplayService implements DisplayService {
     @Override
     public void print(String message) {
-        System.out.println("*********************************");
+        final String border = "*********************************";
+        System.out.println(border);
         System.out.println(message);
-        System.out.println("*********************************");
+        System.out.println(border);
     }
 }
