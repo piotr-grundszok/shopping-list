583a281ec2591b2d7d8d1a04a310ad87b8889d53
Piotr Grundszok <piotr-grundszok@wp.pl>
2020-12-13 13:26:08 +0100

    some more cleanup

--- src/com/grundszok/piotr/app/messages/Messages.java
+++ src/com/grundszok/piotr/app/messages/Messages.java
@@ -1,10 +1,8 @@
 package com.grundszok.piotr.app.messages;
 
 public final class Messages {
-
     public static final String INVALID_LIST = "Twoja lista jest pusta!";
 
     private Messages() {
     }
-
 }
